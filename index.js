81b143897be5fab4179c297cdcbee5131dd5bf40
vishvas-choudhary <vishvaschoudhary7@gmail.com>
2021-05-02 09:24:06 +0530

    set up for heroku

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log("connected to port " + port);
 });
